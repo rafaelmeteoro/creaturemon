317110361e6cb80268ecd5e4cb590f07bd5fdab7
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2019-01-20 13:11:25 -0200

    unit test create crature with strength equals zero

--- app/src/test/java/com/meteoro/creaturemon/viewmodel/CreatureViewModelTest.kt
+++ app/src/test/java/com/meteoro/creaturemon/viewmodel/CreatureViewModelTest.kt
@@ -58,4 +58,15 @@ class CreatureViewModelTest {
         val canSaveCreature = creatureViewModel.canSaveCreature()
         assertEquals(false, canSaveCreature)
     }
+
+    @Test
+    fun testCantSaveCreatureWithoutStrength() {
+        creatureViewModel.intelligence = 10
+        creatureViewModel.strength = 0
+        creatureViewModel.endurance = 7
+        creatureViewModel.drawable = 1
+        creatureViewModel.name = "My Creature"
+        val canSaveCreature = creatureViewModel.canSaveCreature()
+        assertEquals(false, canSaveCreature)
+    }
 }
